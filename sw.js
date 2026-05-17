692a6c0f1f380cb4cc4039551812721cb58986d4
Rick Mosher <rickmosher@Ricks-2021-MacBook-Pro.local>
2026-05-17 15:03:13 -0600

    Bump SkyJo asset cache version

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "dashboard-v69";
+const VERSION = "dashboard-v70";
 const CORE_CACHE = `${VERSION}-core`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const REMOTE_CACHE = `${VERSION}-remote`;
