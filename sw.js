8a724e66195399310e279a3839c922ca749d8377
Rick Mosher <rickmosher@Ricks-2021-MacBook-Pro.local>
2026-05-18 17:23:20 -0600

    Use preset art on card tables

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "dashboard-v75";
+const VERSION = "dashboard-v79";
 const CORE_CACHE = `${VERSION}-core`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const REMOTE_CACHE = `${VERSION}-remote`;
