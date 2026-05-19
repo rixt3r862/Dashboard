1c58030da2d6dd28a4525e4f5ba581e91402cc34
Rick Mosher <rickmosher@Ricks-2021-MacBook-Pro.local>
2026-05-18 18:25:23 -0600

    Retheme Hearts table

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "dashboard-v80";
+const VERSION = "dashboard-v83";
 const CORE_CACHE = `${VERSION}-core`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const REMOTE_CACHE = `${VERSION}-remote`;
