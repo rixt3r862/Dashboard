021da6ac72abd9a581cc808f0067488321f66cbe
Rick Mosher <rickmosher@Ricks-2021-MacBook-Pro.local>
2026-05-22 20:51:58 -0600

    Add Game Room links to games

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "dashboard-v104";
+const VERSION = "dashboard-v105";
 const CORE_CACHE = `${VERSION}-core`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const REMOTE_CACHE = `${VERSION}-remote`;
