f84ace760d6fb8498559460f9744c82278c50ca0
Rick Mosher <rickmosher@Ricks-2021-MacBook-Pro.local>
2026-08-10 20:12:38 -0600

    Let SkyJo phone board use full width

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "dashboard-v110";
+const VERSION = "dashboard-v111";
 const CORE_CACHE = `${VERSION}-core`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const REMOTE_CACHE = `${VERSION}-remote`;
