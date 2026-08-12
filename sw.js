01abf326c4e4b84121945202fa0cdffdc1ac024b
Rick Mosher <rickmosher@Ricks-2021-MacBook-Pro.local>
2026-08-12 08:30:42 -0600

    Add SkyJo Mobile game link

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "dashboard-v113";
+const VERSION = "dashboard-v114";
 const CORE_CACHE = `${VERSION}-core`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const REMOTE_CACHE = `${VERSION}-remote`;
