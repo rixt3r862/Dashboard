718ee4043474ae0e737348505a7f3b5837d0a862
Rick Mosher <rickmosher@Ricks-2021-MacBook-Pro.local>
2026-09-04 09:01:00 -0600

    Refine time converter rounding

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "dashboard-v116";
+const VERSION = "dashboard-v117";
 const CORE_CACHE = `${VERSION}-core`;
 const RUNTIME_CACHE = `${VERSION}-runtime`;
 const REMOTE_CACHE = `${VERSION}-remote`;
